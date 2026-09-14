2e886ff8792e8b26e50e5a229d5e2d3a8027f6f5
PoPomini <osm7660@gmail.com>
2026-09-14 15:59:58 +0900

    feat: add Area spawn and destroy logic
    - Implement logic to spawn Area prefabs
    - Implement logic to destroy Area prefabs when no longer needed

---
+++ ColorRun/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField]
+    private AreaSpawner areaSpawner;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ChangeColorBar"))
+        {
+            areaSpawner.DestroyArea(); //이전 구역 삭제
+            areaSpawner.SpawnArea();  // 새로운 구역 생성
+            Destroy(collision.gameObject); // ChangeColorBar 오브젝트 삭제
+        }
+    }
+}
