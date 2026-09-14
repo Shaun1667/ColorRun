c48a0b64ef3c6201d84440cc1e27d07f86a07f58
alphamon1114 <alphamon1114@gmail.com>
2026-09-14 17:46:44 +0900

    ca

--- ColorRun/Assets/Scripts/AreaSpawner.cs
+++ ColorRun/Assets/Scripts/AreaSpawner.cs
@@ -6,6 +6,10 @@ public class AreaSpawner : MonoBehaviour
     private readonly float areaLength = 18;
 
     [SerializeField]
+    private Color[] colors;
+
+
+    [SerializeField]
     private GameObject[] areaPrefabs;
     [SerializeField]
     private Transform player;
@@ -14,21 +18,25 @@ public class AreaSpawner : MonoBehaviour
     private void Awake()
     {
         SpawnArea();
-
     }
+
     public void SpawnArea()
     {
-        int index = Random.Range(0, areaPrefabs.Length);
-        Vector3 position = Vector3.up * areaLength * areaIndex;
-        Instantiate(areaPrefabs[index], position, Quaternion.identity, transform);
+        int index = Random.Range(0, areaPrefabs.Length); // 0~4 0,1,2,3
+        Vector3 position = Vector3.up * areaLength * areaIndex; //(0,1,0)*18*0~++
+        GameObject area = Instantiate(areaPrefabs[index], position, Quaternion.identity, transform);
+        area.GetComponent<AreaController>().Setup(colors);
+
         areaIndex++;
     }
+
     public void DestroyArea()
     {
-        for(int i = 0; i < transform.childCount; ++i)
+        for (int i = 0; i < transform.childCount; ++i)
         {
             Transform child = transform.GetChild(i);
-            if(player.position.y - child.position.y >= areaLength)
+
+            if (player.position.y - child.position.y >= areaLength)
             {
                 Destroy(child.gameObject);
             }
---
+++ ColorRun/Assets/Scripts/Utils.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+public static int[] RandomNumerics(int maxCount, int n)
+    {
+        int[] defaults = new int[maxCount];
+        int[] results = new int[n];
+
+        for(int i =0; i<maxCount; ++i)
+        {
+            defaults[i] = i;
+        }
+        for(int i = 0; i < n; ++i)
+        {
+            int index = Random.Range(0, maxCount);
+            results[i] = defaults[index];
+            defaults[index] = defaults[maxCount - 1];
+            maxCount--;
+        }
+        return results;
+    }
+}
