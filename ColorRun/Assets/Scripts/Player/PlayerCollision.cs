70e8dafa03f58342f35e1a07fdcf6b97af72bc04
naminjae <alslxhrl7@gmail.com>
2026-09-14 17:51:25 +0900

    save-mine

--- ColorRun/Assets/Scripts/Player/PlayerCollision.cs
+++ ColorRun/Assets/Scripts/Player/PlayerCollision.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
+public class pla : MonoBehaviour
 {
-
+    //이전 구역 삭제...
+    //출돌한 물체를 삭제...
 
     [SerializeField]
     private AreaSpawner areaSpawner;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("ChangeColorBar"))
         {
-            //이전 구역 삭제...
+            //이전 구역 삭제
+
             areaSpawner.DestroyArea();
             //구역 생성
             areaSpawner.SpawnArea();
-            //충돌한 물체를 삭제...
+            //충돌한 물체 삭제
             Destroy(collision.gameObject);
         }
+
+
     }
-}
+    }
+
