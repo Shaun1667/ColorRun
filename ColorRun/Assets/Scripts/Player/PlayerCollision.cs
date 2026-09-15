998848cb7d06e8e1c3e6e9543e6e6279b7de0338
wkdjm00 <80094221+wkdjm00@users.noreply.github.com>
2026-09-15 16:34:27 +0900

    feat:플레이어*오브젝트 색상 매칭 판정 및 효과음 추가
    feat:플레이어 와 오브젝트 색상 매칭 판정 및 효과음 추가

--- ColorRun/Assets/Scripts/Player/PlayerCollision.cs
+++ ColorRun/Assets/Scripts/Player/PlayerCollision.cs
@@ -2,21 +2,62 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    //이전 구역을 삭제
-    //충돌한 물체를 삭제
+    [Header("Audio Clips")]
+    [SerializeField]
+    private AudioClip[] clips;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    [SerializeField]
+    private PlayerColor playerColor;
 
     [SerializeField]
     private AreaSpawner areaSpawner;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("ChangeColorBar"))
         {
-            //이전 물체를삭제
-            //충돌한 구역을 삭제
-            areaSpawner.SpawnArea();
+            PlaySound(0);
+            //플레이어 색상 변경
+            playerColor.SetColor(collision.GetComponent<ChangeColorBarController>().CurrentColor);
+            //이전 구역 삭제...
             areaSpawner.DestroyArea();
+            //구역 생성
+            areaSpawner.SpawnArea();
+            //충돌한 물체를 삭제...
             Destroy(collision.gameObject);
         }
+
+        if (collision.CompareTag("Item"))
+        {
+            PlaySound(1);
+            Destroy(collision.gameObject);
+        }
+
+        if(collision.CompareTag("Obstacle"))
+        {
+            if(collision.GetComponent<SpriteRenderer>().color == playerColor.CurrentColor)
+            {
+                PlaySound(2);
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                PlaySound(3);
+                //todo 게임오버
+                Debug.Log("Player Die");
+            }
+        }
+    }
+
+    public void PlaySound(int index)
+    {
+        audioSource.PlayOneShot(clips[index]);
     }
 }
