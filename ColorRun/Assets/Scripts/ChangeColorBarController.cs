7f91f5366b0387534a4f1174c98f153fa60e90ae
naminjae <alslxhrl7@gmail.com>
2026-09-15 16:36:23 +0900

    up

--- ColorRun/Assets/Scripts/ChangeColorBarController.cs
+++ ColorRun/Assets/Scripts/ChangeColorBarController.cs
@@ -1,54 +1,57 @@
-    using System.Collections;
-    using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
-    public class ChangeColorBarController : MonoBehaviour
+public class ChangeColorBarController : MonoBehaviour
+{
+    private SpriteRenderer[] renderers;
+    private float fadeTime = 0.4f;
+
+    public Color CurrentColor { get; private set; }
+
+    private void Awake()
     {
-        private SpriteRenderer[] renderers;
-        private float fadeTime = 0.4f;
+        renderers = transform.GetComponentsInChildren<SpriteRenderer>();
+    }
 
-        private void Awake()
-        {
-            renderers = transform.GetComponentsInChildren<SpriteRenderer>();
-        }
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(OnFadeLoop));
+    }
 
-        private void OnEnable()
-        {
-            StartCoroutine(nameof(OnFadeLoop));
-        }
+    private void OnDisable()
+    {
+        StopCoroutine(nameof(OnFadeLoop));
+    }
 
-        private void OnDisable()
-        {
-            StopCoroutine(nameof(OnFadeLoop));
-        }
+    IEnumerator OnFadeLoop()
+    {
+        //yield return new WaitForSeconds(fadeTime);
+        var waitForSeconds = new WaitForSeconds(fadeTime);
 
-        IEnumerator OnFadeLoop()
-        {
-            //yield return new WaitForSeconds(fadeTime);
-            var waitForSeconds = new WaitForSeconds(fadeTime);
-
-            while (true)
-            {
-                for (int i = 0; i < renderers.Length; i++)
-                {
-                    StartCoroutine(FadeEffect.Fade(renderers[i], 1f, 0f, fadeTime));
-                }
-
-                yield return waitForSeconds;
-
-                for (int i = 0; i < renderers.Length; i++)
-                {
-                    StartCoroutine(FadeEffect.Fade(renderers[i], 0f, 1f, fadeTime));
-                }
-
-                yield return waitForSeconds;
-            }
-        }
-
-        public void SetColor(Color color)
+        while (true)
         {
             for (int i = 0; i < renderers.Length; i++)
             {
-                renderers[i].color = color;
+                StartCoroutine(FadeEffect.Fade(renderers[i], 1f, 0f, fadeTime));
             }
+
+            yield return waitForSeconds;
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                StartCoroutine(FadeEffect.Fade(renderers[i], 0f, 1f, fadeTime));
+            }
+
+            yield return waitForSeconds;
         }
     }
+
+    public void SetColor(Color color)
+    {
+        CurrentColor = color;
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].color = color;
+        }
+    }
+}
