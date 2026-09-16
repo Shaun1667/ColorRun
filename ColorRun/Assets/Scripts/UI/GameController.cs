180e8963408f3aaebbcaafd21c6f934185e21327
pmj713 <sub07007@gmail.com>
2026-09-16 09:10:48 +0900

    타이틀/시작 화면 구현
    - GameController: 첫 클릭 전까지 대기, 클릭 시 게임 시작(IsGamePlay = true)
    - UIController: 게임 시작 시 메인(타이틀) 패널 비활성화
    - UITextBlink: TextMeshProUGUI 텍스트 반복 페이드(깜빡임) 연출
    - FadeEffect에 TextMeshProUGUI 오버로드 추가
    - PlayerMovement: 게임 시작 전에는 이동 입력 무시
    - TextMeshPro 패키지 및 한글 폰트(NotoSansKR-Bold) 추가, 타이틀 텍스트에 적용

---
+++ ColorRun/Assets/Scripts/UI/GameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private UIController uiController;
+
+    public bool IsGamePlay { get; private set; } = false;
+
+    private IEnumerator Start()
+    {
+        while(true)
+        {
+            if (Input.GetMouseButtonUp(0)) 
+            {
+                GameStart();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    private void GameStart()
+    {
+        IsGamePlay = true;
+        uiController.GameStart();
+    }
+}
---
+++ ColorRun/Assets/Scripts/UI/UITextBlink.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class UITextBlink : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeTime = 0.4f;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(OnBlink));
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(nameof(OnBlink));
+    }
+
+    private IEnumerator OnBlink()
+    {
+        while(true)
+        {
+            yield return StartCoroutine(FadeEffect.Fade(text, 1, 0, fadeTime));
+            yield return StartCoroutine(FadeEffect.Fade(text, 0, 1, fadeTime));
+        }
+    }
+}
