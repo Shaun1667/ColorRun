2c82ff3258c70da770b34dab1792064e29694ccf
wkdjm00 <80094221+wkdjm00@users.noreply.github.com>
2026-09-16 17:57:05 +0900

    게임 점수 추가, 게임오버 UI제작및 게임오버 시스템과 이펙트 완료
    게임 점수 추가, 게임오버 UI제작및 게임오버 시스템과 이펙트 완료

---
+++ ColorRun/Assets/Prefabs/UI/GameController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerData playerData;
+    [SerializeField]
+    private UIController uiController;
+
+    public bool IsGamePlay { get; private set; } = false;
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                GameStart();
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void GameStart()
+    {
+        IsGamePlay = true;
+        uiController.GameStart();
+    }
+
+    public void GameOver()
+    {
+        IsGamePlay = false;
+        StartCoroutine(nameof(OnGameOver));
+    }
+
+    private IEnumerator OnGameOver()
+    {
+        float percent = 0;
+        float time = 1.5f;
+
+        while (percent < time)
+        {
+            percent += Time.deltaTime;
+            yield return null;
+        }
+
+        bool isBestScore = false;
+
+        int bestScore = PlayerPrefs.GetInt(Constants.BESTSCORE);
+
+        if (bestScore < playerData.CurrentScore)
+        {
+            isBestScore = true;
+            bestScore = playerData.CurrentScore;
+            PlayerPrefs.SetInt(Constants.BESTSCORE, bestScore);
+        }
+
+        //게임오버 UI 출력
+        uiController.GameOver(playerData.CurrentScore, bestScore, isBestScore);
+    }
+
+}
---
+++ ColorRun/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.Rendering.DebugUI;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent<int> onChagedCurrentScore;
+
+    private int currentScore = 0;
+    public int CurrentScore
+    {
+        set
+        {
+            currentScore = value;
+            onChagedCurrentScore?.Invoke(currentScore);
+        }
+        get => currentScore;
+
+    }
+}
