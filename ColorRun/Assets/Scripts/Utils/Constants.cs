9ac0c527bdfb0ba8e1e86a7dae1d61b0cc7dee5e
Yewon Jeong <yewn.jeong@gmail.com>
2026-09-18 18:00:11 +0900

    상점 구매 설정

--- ColorRun/Assets/Scripts/Utils/Constants.cs
+++ ColorRun/Assets/Scripts/Utils/Constants.cs
@@ -4,6 +4,8 @@ public class Constants
 {
     public static readonly string BESTSCORE = "BESTSCORE";
     public static readonly string STARCOUNT = "STARCOUNT";
-    public static readonly string SKIN_PATH = "Skins/";
+    public static readonly string SKIN_PATH = "Skin/";
+    public static readonly string IS_OWNED_SKIN_ = "IS_OWEND_SKIN_";
+    public static readonly string SELECTED_SKIN_INDEX = "SELECTED_SKIN_INDEX";
 
 }
